ad8b5f13fd59b4471cb1e0d7f35e5cad08c57cc6
zakariak <zkzk@live.nl>
2018-01-11 09:34:12 +0100

    whack-a-mole
    whack-a-mole

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var holes = document.querySelectorAll('.hole');
+var scoreBoard = document.querySelector('.score');
+var moles = document.querySelectorAll('.mole');
+
+var startButton = document.querySelector('[data-action="startGame"]');
+
+startButton.addEventListener('click', function () {
+    startGame();
+});
+
+
+function startGame() {
+    
+}
